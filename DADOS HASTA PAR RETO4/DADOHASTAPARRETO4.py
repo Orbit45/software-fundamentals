5a04aba00270adaaa61e692cf3a92bfa53299965
OrbitTecnologia2 <sebasmoragarcia1004@gmail.com>
2025-11-15 14:52:57 -0500

    software-fundamentals

--- DADOS HASTA PAR RETO4/DADOHASTAPARRETO4.py
+++
@@ -1,9 +0,0 @@
-import random
-
-while True:
-    d1 = random.randint(1, 6)
-    d2 = random.randint(1, 6)
-    print("Dado 1:", d1, " Dado 2:", d2)
-    if d1 == 6 and d2 == 6:
-        print("¡Par de SEIS generado!")
-        break
